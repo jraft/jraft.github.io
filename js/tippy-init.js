4bbe9ba3da6eb6f1633fc7c49339fb73ffe368d6
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-10-13 20:09:20 +0800

    add js files

---
+++ js/tippy-init.js
@@ -0,0 +1,9 @@
+tippy('.tippy', {             // Use class or id
+  animation: 'fade',         // See docs for more options (there are a few nice ones 😉)
+  duration: 200,              // Duration for ToolTip Animation
+  arrow: true,                // Add ToolTip Arrow
+  delay: [100, 400],             // First # = delay in, second # = delay out
+  arrowType: 'round',         // Sharp or 'round' or remove for none
+  theme: 'light',             // Dark is the default
+  maxWidth: 220,              // Max width in pixels for the tooltip
+})
